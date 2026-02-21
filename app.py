c6634ff0ca950d67ebbeecb78be23e747944e885
DkXenos <superjason101010@gmail.com>
2026-02-22 00:04:02 +0700

    input file

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+import webbrowser
+from pick import pick
+
+def main():
+    items = {
+        "Youtube": "https://www.youtube.com",
+        "Github": "https://www.github.com",
+        "Linkedin": "https://www.linkedin.com",
+        "Gemini": "https://gemini.google.com",
+        "Safari": "https://www.apple.com/safari/",
+        "Google": "https://www.google.com",
+        "GDocs": "https://docs.google.com",
+        "Visual Studio Code": "Visual Studio Code",
+        "Xcode": "Xcode",
+        "Unity Hub": "Unity Hub"
+    }
+
+    options = list(items.keys())
+    title = 'Select what you want to open:'
+    option, index = pick(options, title, indicator='>', default_index=0)
+
+    selection = items[option]
+
+    if selection.startswith("http"):
+        print(f"Opening browser to {option}...")
+        webbrowser.open(selection)
+    else:
+        print(f"Launching {selection}...")
+        os.system(f'open -a "{selection}"')
+
+if __name__ == "__main__":
+    main()
